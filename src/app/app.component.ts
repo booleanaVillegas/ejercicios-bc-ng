d3602b873d610291dea7558912f72526a801a4a1
Juliana Villegas <jumavipe@hotmail.com>
2020-04-21 16:21:54 -0500

    se adiciona ejecicio de biografia con scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rick-biography';
+  title = 'Rick Sánchez';
+  bio = 'Rick Sanchez is the titular anti-heroic main protagonist of the "Rick and Morty" series. He is a genius scientist whose alcoholism and reckless, nihilistic behavior are a source of concern for his daughter\'s family, as well as the safety of their son, Morty. He is voiced by Justin Roiland. He is later revealed to be Morty\'s cousin in an interview with Roiland himself.';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoItemComponent } from './info-item/info-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoItemComponent
   ],
   imports: [
     BrowserModule,
